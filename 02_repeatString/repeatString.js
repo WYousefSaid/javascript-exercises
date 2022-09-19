db9233dce5a62d6ac32abd0c1aef4f6b2ae7598c
WYousefSaid <WYousefSaid@proton.me>
2022-09-19 18:53:54 +0200

    finished 01 && 02 exersices

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+const repeatString = function(word,items) {
+   if (items<0) return 'ERROR';
+   let string = '';
+   for (let i = 0; i < items; i++){
+      string += word;
+   }
+   return string;
 
 };
-
 // Do not edit below this line
 module.exports = repeatString;
