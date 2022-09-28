03bb7dc212ae98403c4808ae9b7eaf98a587a9b8
WYousefSaid <wyousefsaid@proton.me>
2022-09-28 19:21:39 +0200

    finished 10 exersice

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    return n < 1 ? "OOPS"
+        : n <= 2 ? 1
+            : fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
