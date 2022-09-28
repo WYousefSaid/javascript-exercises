2a0108512906a1254a2e33ecede8dda2fe79b669
WYousefSaid <wyousefsaid@proton.me>
2022-09-28 19:10:34 +0200

    finished 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    var re = /[\W_]/g
+    const str = string.toLowerCase().replace(re,'');
+    const strreversed = str.split('').reverse().join('');
+    return str === strreversed;
 };
 
 // Do not edit below this line
