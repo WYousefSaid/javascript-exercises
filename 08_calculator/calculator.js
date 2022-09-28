f216f6f11bae821f32d7001131d347b55660601c
WYousefSaid <wyousefsaid@proton.me>
2022-09-28 18:42:17 +0200

    finished 08 exersice

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,49 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+   let sum = 0;
+    for (let b = 0; b < a.length; b++) {
+        sum += a[b];
+
+    }
+    return sum;
 };
 
-const multiply = function() {
+const multiply = function (a) {
+    const total = a.reduce((accumulator, current) => accumulator*current,1);
+    return total;
+};
+
+const power = function (a,b) {
+    return Math.pow(a,b);
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (num) {
+    if (num === 0){
+        return 1;
+    }
+    else if (num > 0){
+        let fact = 1;
+        for (i = 1; i<=num; i++){
+            fact *= i;
+        }
+        return fact;
+    }
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
